3c866f5c2456464cd4186ba088eabe6bfe13d8de
Fer Torres <fertorresmx@gmail.com>
2020-12-06 15:30:58 -0600

    Create and test Hello World file

---
+++ selenium/hello_world.py
@@ -0,0 +1,26 @@
+import unittest
+from pyunitreport import HTMLTestRunner
+from selenium import webdriver
+
+class HelloWorld(unittest.TestCase):
+
+    def setUp(self):
+        # self.driver = webdriver.Chrome(executable_path = r'C://selenium/chromedriver.exe')
+        self.driver = webdriver.Chrome(executable_path = './../../../chromedriver_dir/87_0_4280_88/chromedriver')
+        driver = self.driver
+
+        # Wait for 10 seconds
+        driver.implicitly_wait(100)
+
+
+    def test_hello_world(self):
+        driver = self.driver
+        driver.get('https://www.platzi.com')
+
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity = 2, testRunner = HTMLTestRunner(output = 'reportes', report_name = 'hello-world-report'))
